0f4e8831b5b3f27c0a302e5d3aa7ffa87606db71
Tyler Long <tyler4long@gmail.com>
2017-01-07 10:18:55 +0800

    Get rid of coffee-script

---
+++ src/adapter.js
@@ -0,0 +1,54 @@
+try {
+  const { Adapter, TextMessage, User } = require('hubot')
+} catch (_) {
+  const prequire = require('parent-require')
+  const { Adapter, TextMessage, User } = prequire('hubot')
+}
+
+const GlipSocket = require('glip.socket.io')
+
+class GlipAdapter extends Adapter {
+  constructor() {
+    super()
+    this.robot.logger.info('Constructor')
+    this.client = new GlipSocket({
+      host: process.env.HUBOT_GLIP_HOST || 'glip.com',
+      port: process.env.HUBOT_GLIP_PORT || 443,
+      user: process.env.HUBOT_GLIP_EMAIL,
+      password: process.env.HUBOT_GLIP_PASSWORD
+    })
+    this.client.on('message', (type, data) => {
+      this.robot.logger.info(`${type} : ${JSON.stringify(data, null, 4)}`)
+      const user = new User(data.creator_id, room = data.group_id, reply_to = data.group_id, name = `User ${data.creator_id} from Group ${data.group_id}`)
+      const message = new TextMessage(user, data.text, 'MSG-' + data._id)
+      this.robot.receive(message)
+    });
+  }
+
+  send(envelope, strings) {
+    this.robot.logger.info('send ' + JSON.stringify(envelope, null, 4) + '\n\n' + strings.join('\n\n'))
+    if (envelope.message_type === 'image_url') { // send image by url
+      strings.forEach((str) => {
+        this.client.post_file_from_url(envelope.user.reply_to, str, '')
+      })
+    }
+    else {
+      this.client.post(envelope.user.reply_to, strings.join('\n'))
+    }
+  }
+
+  reply(envelope, strings) {
+    this.robot.logger.info('reply ' + JSON.stringify(envelope, null, 4) + '\n\n' + strings.join('\n\n'))
+    this.client.post(envelope.user.reply_to, strings.join('\n'))
+  }
+
+  run() {
+    this.robot.logger.info("Run")
+    this.client.start()
+    this.client.on('started', () => {
+      this.emit('connected')
+    })
+  }
+}
+
+module.exports = GlipAdapter
