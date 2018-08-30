dac007a61e18932ea35916e05dbe69c33aa1a6d6
Tyler Long <tyler4long@gmail.com>
2018-08-30 22:34:23 +0800

    Rewrite code

--- src/index.js
+++ src/index.js
@@ -1,49 +1,38 @@
 import RingCentral from 'ringcentral-js-concise'
+import fs from 'fs'
+import PubNub from 'ringcentral-js-concise/src/pubnub'
 
 const { Adapter, TextMessage, User } = global.hubot
 
 class GlipAdapter extends Adapter {
   constructor (robot) {
     super(robot)
-    this.client = new RingCentral(
+    this.rc = new RingCentral(
       process.env.RINGCENTRAL_CLIENT_ID,
       process.env.RINGCENTRAL_CLIENT_SECRET,
       process.env.RINGCENTRAL_SERVER || RingCentral.PRODUCTION_SERVER
     )
 
-    this.client.getToken().then(() => {
+    if (fs.existsSync('./token.json')) {
+      const data = fs.readFileSync('./token.json', 'utf8')
+      this.rc.token(JSON.parse(data))
       this.robot.logger.info('Token restored from file')
-      this._subscribe()
-    }).catch((e) => {
+      this.subscribe()
+    } else {
       this.robot.logger.error('No saved token detected. You need to add the bot to Glip first.')
-    })
+    }
 
-    this.robot.router.get('/oauth', (req, res) => {
-      if (!req.query.code) {
-        res.status(500)
-        res.send({ 'Error': "Looks like we're not getting code." })
-        this.robot.logger.error('Looks like we are not getting code.')
-        return
-      }
-      this.robot.logger.info(req.query.code)
-      this.client.oauth(req.query.code, `${process.env.RINGCENTRAL_BOT_SERVER}/oauth`).then(() => {
-        res.status(200)
-        res.send('success')
-        this.robot.logger.info('oauth is successful')
-        this._subscribe()
-      }).catch(e => {
-        res.status(500)
-        res.send(e.message)
-        this.robot.logger.error(e)
-      })
+    this.robot.router.get('/oauth', async (req, res) => {
+      await this.rc.authorize({ code: req.query.code, redirect_uri: process.env.RINGCENTRAL_BOT_SERVER + '/oauth' })
+      fs.writeFileSync('./token.json', JSON.stringify(this.rc.token(), null, 2))
+      this.subscribe()
     })
   }
 
-  _subscribe () {
+  async subscribe () {
     this.emit('connected')
-    const subscription = this.client.createSubscription()
-    subscription.onMessage(message => {
-      this.robot.logger.info(JSON.stringify(message, null, 4))
+    const pubnub = new PubNub(this.rc, ['/restapi/v1.0/glip/posts'], message => {
+      this.robot.logger.info(JSON.stringify(message, null, 2))
       const post = message.body
       if (post.eventType === 'PostAdded' && post.text && post.text !== '') {
         const user = new User(post.creatorId, {
@@ -55,21 +44,18 @@ class GlipAdapter extends Adapter {
         this.robot.receive(hubotMessage)
       }
     })
-    subscription.subscribe(['/glip/posts']).then(subscription => {
-      this.robot.logger.info('Subscription created')
-    }, e => {
-      this.robot.logger.error(e)
-    })
+    await pubnub.subscribe()
+    this.robot.logger.info('Subscription created')
   }
 
   send (envelope, string) {
     this.robot.logger.info('send ' + JSON.stringify(envelope, null, 4) + '\n\n' + string)
-    this.client.glip().posts().post({ groupId: envelope.user.reply_to, text: string })
+    this.rc.glip().posts().post({ groupId: envelope.user.reply_to, text: string })
   }
 
   reply (envelope, string) {
     this.robot.logger.info('reply ' + JSON.stringify(envelope, null, 4) + '\n\n' + string)
-    this.client.glip().posts().post({ groupId: envelope.user.reply_to, text: string })
+    this.rc.glip().posts().post({ groupId: envelope.user.reply_to, text: string })
   }
 
   run () {
